7a6bc46dd8be6ecfecd51a9a72d0cf3d1b09cfc3
Rajcasp94 <30457754+Rajcasp94@users.noreply.github.com>
2024-07-02 14:05:18 +0300

    Add files via upload

---
+++ testappver02.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+#import neurokit2 as nk
+import scipy
+#from scipy.signal import find_peaks, medfilt
+#import neurokit2 as nk
+from statistics import mode
+#import scipy.signal as signal
+from scipy import stats
+
+st.title('Get your PAT')
+
+sample_rate = st.number_input('Sampling rate (Hz)')
+
+st.write('sample rate is', sample_rate)
